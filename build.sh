dcc6aca66bae575356fd8c4f1edfb647e484f0d6
Rui-Zhi Li (李睿智) <61745903+Astro-Lee@users.noreply.github.com>
2024-05-01 16:08:49 +0800

    download index data

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # https://astrometry.net/doc/build-index.html
 # http://astrometry.net/doc/readme.html#getting-index-files
 # http://data.astrometry.net/
 # download index files to /usr/local/astrometry/data OR
-# make install-indexes
+
+# index-5201-*
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5201-$I.fits
+done
+
+# index-5203-*
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5203-$I.fits
+done
+
+# index-5205-*
+for ((i=0; i<48; i++)); do
+    I=$(printf %02i $i)
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5205-$I.fits
+done
