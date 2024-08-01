83bc870c393df88884739fc768819dcdc319641d
Rui-Zhi Li (李睿智) <61745903+Astro-Lee@users.noreply.github.com>
2024-08-01 23:19:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,3 +4,5 @@ set -e
 /opt/miniconda3/bin/watchmedo auto-restart -d stdweb -p '*.py' --ignore-patterns='*/.*' -- /opt/miniconda3/bin/python -m celery --broker=redis://redis:6379/ -A stdweb worker --loglevel=info > watchmedo.log 2>&1 &
 
 /opt/miniconda3/bin/python manage.py runserver 0.0.0.0:8000
+
+jupyter-notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
