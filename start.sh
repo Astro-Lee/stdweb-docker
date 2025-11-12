69a44eb77ec7968ff5ee54754cdb1c2441b2c751
Rui-Zhi Li (李睿智) <61745903+Astro-Lee@users.noreply.github.com>
2025-11-12 16:48:40 +0800

    Update start.sh to run JupyterLab and Celery in background

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-#/opt/conda3/bin/jupyter-lab --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
+# 启动 JupyterLab（后台运行）
+/opt/conda3/bin/jupyter-lab \
+  --notebook-dir=/opt/notebooks \
+  --ip=0.0.0.0 \
+  --port=8888 \
+  --no-browser \
+  --allow-root > jupyter.log 2>&1 &
 
-/opt/conda3/bin/watchmedo auto-restart -d stdweb -p '*.py' --ignore-patterns='*/.*' -- /opt/miniconda3/bin/python -m celery --broker=redis://redis:6379/ -A stdweb worker --loglevel=info > watchmedo.log 2>&1 &
+# 启动 Celery（后台运行）
+/opt/conda3/bin/watchmedo auto-restart \
+  -d stdweb -p '*.py' --ignore-patterns='*/.*' \
+  -- /opt/conda3/bin/python -m celery --broker=redis://redis:6379/ -A stdweb worker --loglevel=info > watchmedo.log 2>&1 &
 
+# 启动 Django（前台运行）
 /opt/conda3/bin/python manage.py runserver 0.0.0.0:8000
